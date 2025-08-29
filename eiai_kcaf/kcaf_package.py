f8bebf6190dee9f9567538f1147a870e2b0a7918
Kevin Cao <kevincao@au1.ibm.com>
2025-08-29 11:23:25 +1000

    feat/issue_11

--- eiai_kcaf/kcaf_package.py
+++ eiai_kcaf/kcaf_package.py
@@ -5,7 +5,7 @@ class KcafConfig:
     processors_package: str
 
     def __init__(self):
-        processors_package = os.environ.get('EIAI_ KCAF_PROCESSORS_PACKAGE')
+        processors_package = os.environ.get('EIAI_KCAF_PROCESSORS_PACKAGE')
         if processors_package is not None and processors_package != '':
             self.processors_package = processors_package
         else:
